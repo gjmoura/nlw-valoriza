ce171bb6e8a83af5e2df5cd36c8f2c3a537affd7
Gustavo Jeronimo <gustavo.jeronimo@hotmail.com>
2021-07-29 20:28:20 -0300

    Organização dos arquivos do programa

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata";
+import express from "express";
+
+import "./database";
+
+// @types/express
+const app = express();
+
+// http://localhost:3000
+app.listen(3000, () => {
+  console.log("[SERVIDOR RODANDO]");
+});
