8a8774ecbd50400cffef110f3d3fd82f60ba45c1
Gustavo Jeronimo <gustavo.jeronimo@hotmail.com>
2021-07-31 18:45:31 -0300

    Finaliza a segunda aula

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import "reflect-metadata";
 import express from "express";
 
+import { router } from "./routes";
+
 import "./database";
 
 // @types/express
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 // http://localhost:3000
 app.listen(3000, () => {
